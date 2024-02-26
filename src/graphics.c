ff8c2f3c31071bb76b32c144b83d75bb3f433c0a
chuckgarcian <chuckgarcian@gmail.com>
2024-02-26 07:39:28 -0600

    add pausing

--- src/graphics.c
+++ src/graphics.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#include <bits/pthreadtypes.h>
+
 #include "raylib.h"
 #include "graphics.h"
 
-#define WD_COLS 32
-#define WD_ROWS 32
 #define WD_K 32
 #define CELL_SIZE 16
 #define SCRNW ((512 * 3) + 100)
 #define SRCHT 512
 #define FPS 1
 
+/* Matric Size */
+#define WD_COLS 32
+#define WD_ROWS 32
+
+/* Submatrices Block Size */
 #define MR 2
 #define NR 2
 #define KC 4
-#define MC 8
+#define MC 4 
 #define NC 8
 
-// Function prototypes
+int pause;
+
+void *handleKeyInput (void *ptr);
+void gemm();
+
 static void drawBlock(int col, int row, int posX, int posY, int scaleC, int scaleR, Color color);
 static void drawGrid(size_t posx, size_t posy, Color color);
-
-void gemm() 
-{
-  for (int jr = 0; jr < NC; jr += NR) 
-  {
-    for (int ir = 0; ir < MC; ir += MR) 
-    {
-      // Empty loop body
-    }
-  }
-}
 
 int main(void)
 {
   InitWindow(SCRNW, SRCHT, "Agent Simulation");
   SetTargetFPS(FPS);
+  pause = 0;
+
+  pthread_t thread_id; 
+  pthread_create(&thread_id, NULL, handleKeyInput, NULL);
 
   for (int j = 0; j < WD_COLS; j += NC) 
   {
@@ -42,6 +47,15 @@ int main(void)
     {
       for (int i = 0; i < WD_ROWS; i += MC)  
       {
+        
+        
+        while (pause == 1) 
+        {
+          BeginDrawing();         
+          DrawText("PAUSED", 0, 10, 100, RED);
+          EndDrawing();          
+        }
+        
         BeginDrawing();         
         ClearBackground(RAYWHITE);
 
@@ -55,9 +69,8 @@ int main(void)
         
         drawBlock(j, 0, 68, 0, NC, 32, GREEN);
         drawBlock(j, p, 68, 0, NC, KC, DARKGREEN);
-        drawGrid(68, 0, DARKGREEN); 
-        
-        EndDrawing();          
+        drawGrid(68, 0, DARKGREEN);
+        EndDrawing();         
       }
     }
   }
@@ -81,3 +94,26 @@ static void drawGrid(size_t posx, size_t posy, Color color)
     }
   }
 }
+
+void *handleKeyInput (void *ptr)
+{
+ while (1) 
+ {
+  if (IsKeyPressed(KEY_SPACE)) 
+      pause = (pause == 0) ? 1 : 0;
+  else if (IsKeyPressed(KEY_Q))
+      exit(0);
+ }
+}
+
+void gemm() 
+{
+  for (int jr = 0; jr < NC; jr += NR) 
+  {
+    for (int ir = 0; ir < MC; ir += MR) 
+    {
+      // Empty loop body
+    }
+  }
+}
+
